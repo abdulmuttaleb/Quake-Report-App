c787479e5ab20cd6fefcbf449a426248ac8067cc
Saiko <a.m.abdelmottaleb@gmail.com>
2018-03-06 15:59:17 +0200

    first commit

---
+++ app/src/main/java/com/isaiko/quakereportapp/Quake_MainActivity.java
@@ -0,0 +1,13 @@
+package com.isaiko.quakereportapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Quake_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quake__main);
+    }
+}
