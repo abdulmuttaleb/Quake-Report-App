cb60f19cd482fcc8756bb70974ebec4564ef344d
Saiko <a.m.abdelmottaleb@gmail.com>
2018-03-12 18:39:54 +0200

    added preferences - inserting the minimum magnitude to fetch data by - added sorting by magnitude or most recent

--- app/src/main/java/com/isaiko/quakereportapp/Quake_MainActivity.java
+++ app/src/main/java/com/isaiko/quakereportapp/Quake_MainActivity.java
@@ -4,13 +4,17 @@ import android.app.LoaderManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.app.LoaderManager.LoaderCallbacks;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -22,7 +26,7 @@ import java.util.List;
 
 public class Quake_MainActivity extends AppCompatActivity  implements LoaderCallbacks<List<Earthquake>>{
 
-    private final String USGS_REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
+    private final String USGS_REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query";
 
     private static final int EARTHQUAKE_LOADER_ID = 1;
 
@@ -38,7 +42,8 @@ public class Quake_MainActivity extends AppCompatActivity  implements LoaderCall
     public Loader<List<Earthquake>> onCreateLoader(int i, Bundle bundle) {
         Log.d(LOG_TAG, "onCreateLoader");
 
-        return new EarthquakeLoader(this, USGS_REQUEST_URL);
+        String queryURL = formatQueryLink();
+        return new EarthquakeLoader(this, queryURL);
 
     }
 
@@ -117,10 +122,48 @@ public class Quake_MainActivity extends AppCompatActivity  implements LoaderCall
         }
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_settings){
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
     private void openURL(String url){
         Intent i = new Intent(Intent.ACTION_VIEW);
         i.setData(Uri.parse(url));
         startActivity(i);
     }
 
+    private String formatQueryLink(){
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        String minMagnitude = sharedPrefs.getString(getString(
+                R.string.settings_min_magnitude_key),
+                getString(R.string.settings_min_magnitude_default));
+
+        String orderBy = sharedPrefs.getString(
+                getString(R.string.settings_order_by_key),
+                getString(R.string.settings_order_by_default)
+        );
+        Uri baseUri = Uri.parse(USGS_REQUEST_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        uriBuilder.appendQueryParameter("format", "geojson");
+        uriBuilder.appendQueryParameter("limit", "10");
+        uriBuilder.appendQueryParameter("minmag", minMagnitude);
+        uriBuilder.appendQueryParameter("orderby", orderBy);
+
+        return uriBuilder.toString();
+    }
 }
